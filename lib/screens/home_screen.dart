25f63a9e8c0004ff3b2935de80e4795f0d7d7e86
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-05-25 00:54:13 +0530

    Added pie chart after so much struggle XD

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,8 @@
-import 'package:covisafe/models/news.dart';
 // import 'package:covisafe/models/region.dart';
 import 'package:covisafe/models/summary.dart';
-import 'package:covisafe/models/world.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:covisafe/widgets/pie_chart.dart';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,17 +13,29 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   Summary covidSummary;
   bool isLoading = false;
+
+  num total;
+  num confirmedCasesIndian;
+  num confirmedCasesForeign;
+  num discharged;
+  num deaths;
+  num confirmedButLocationUnidentified;
+
   loadSummaryData() async {
-    // setState(() {
-    //   isLoading = true;
-    // });
-    await World.getWorldData();
-    // // covidSummary = await Summary.getSummaryData();
-    // // await Region.getRegionalData();
-    // await News.getNewsData();
-    // setState(() {
-    //   isLoading = false;
-    // });
+    setState(() {
+      isLoading = true;
+    });
+    covidSummary = await Summary.getSummaryData();
+    total = covidSummary.total;
+    confirmedCasesIndian = covidSummary.confirmedCasesIndian;
+    confirmedCasesForeign = covidSummary.confirmedCasesForeign;
+    discharged = covidSummary.discharged;
+    deaths = covidSummary.deaths;
+    confirmedButLocationUnidentified =
+        covidSummary.confirmedButLocationUnidentified;
+    setState(() {
+      isLoading = false;
+    });
   }
 
   @override
@@ -34,15 +47,47 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : Container(
-              child: Center(
-                child: Text('Hello'),
-              ),
-            ),
-    );
+        body: isLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : HomeScreenPieChart(
+                seriesList: _createSampleData(),
+                animate: true,
+              ));
   }
+
+  List<charts.Series<PieStats, String>> _createSampleData() {
+    final data = [
+      // new PieStats('Total', total),
+      new PieStats(
+          'Confirmed Indians', confirmedCasesIndian, Color(0xFF000023)),
+      new PieStats(
+          'Confirmed Foreigners', confirmedCasesForeign, Color(0xFFFF0000)),
+      new PieStats('Discharged', discharged, Color(0xFF00FFFF)),
+      new PieStats('Deaths', deaths, Color(0xFF003143)),
+      // new PieStats('Confirmed But Location Unidentified',
+      //     confirmedButLocationUnidentified),
+    ];
+
+    return [
+      new charts.Series<PieStats, String>(
+        id: 'Covid Summary',
+        data: data,
+        domainFn: (PieStats stat, _) => stat.title,
+        measureFn: (PieStats stat, _) => stat.count,
+        colorFn: (PieStats stat, _) =>
+            charts.ColorUtil.fromDartColor(stat.color),
+        labelAccessorFn: (PieStats row, _) => '${row.count}',
+      )
+    ];
+  }
+}
+
+class PieStats {
+  final String title;
+  final num count;
+  final Color color;
+
+  PieStats(this.title, this.count, this.color);
 }
---
+++ lib/widgets/pie_chart.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+
+class HomeScreenPieChart extends StatelessWidget {
+  final List<charts.Series> seriesList;
+  final bool animate;
+
+  const HomeScreenPieChart({this.seriesList, this.animate});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 500,
+      child: charts.PieChart(
+        seriesList,
+        animate: animate,
+      
+        defaultRenderer: new charts.ArcRendererConfig(arcRendererDecorators: [
+          new charts.ArcLabelDecorator(
+              labelPosition: charts.ArcLabelPosition.auto)
+        ]),
+      ),
+    );
+  }
+}
