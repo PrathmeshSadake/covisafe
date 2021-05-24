d1b8a39f9b9deb28180873d8d4f4c7ae5fc35a61
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-05-25 01:01:37 +0530

    custom stylings

--- lib/utils/styles.dart
+++ lib/utils/styles.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 
 class Styles {
-  static const TextStyle newsTitle = TextStyle(
+  static const TextStyle heading = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.bold,
     color: Colors.grey,
   );
-  static const TextStyle newsDesc = TextStyle(
+  static const TextStyle title = TextStyle(
     fontSize: 17,
-    fontWeight: FontWeight.w900,
+    fontWeight: FontWeight.w800,
+    color: Colors.black,
   );
-  static const TextStyle newsContent = TextStyle(
+  static const TextStyle title2 = TextStyle(
+    fontSize: 17,
+    fontWeight: FontWeight.w500,
+    color: Colors.black,
+  );
+  static const TextStyle content = TextStyle(
     fontSize: 16,
+    fontWeight: FontWeight.w400,
     color: Colors.black87,
   );
 
-  static const TextStyle newsDateSrc = TextStyle(
+  static const TextStyle dateAndSrc = TextStyle(
     fontSize: 15,
     fontWeight: FontWeight.w500,
     color: Colors.grey,
