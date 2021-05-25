bc114980d254a55ecaaf8056434f0115c009abc8
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-05-25 17:17:30 +0530

    legend conatiner containing legends

--- lib/widgets/legends_container.dart
+++ lib/widgets/legends_container.dart
@@ -10,6 +10,26 @@ class LegendsContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 15,
+      ),
+      decoration: BoxDecoration(
+        // border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10.0),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3), //color of shadow
+            spreadRadius: 2, //spread radius
+            blurRadius: 10, // blur radius
+            offset: Offset(0, 1), // changes position of shadow
+            //first paramerter of offset is left-right
+            //second parameter is top to down
+          ),
+          //you can set more BoxShadow() here
+        ],
+      ),
       child: Column(
         children: [
           Legend(
